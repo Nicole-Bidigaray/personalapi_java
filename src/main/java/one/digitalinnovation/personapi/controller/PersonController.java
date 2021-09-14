8daf25097ebacfaf9b468a7de2cf11b6f107bfe3
Marcelo Tedesco <nicole_natsume@yahoo.com.br>
2021-09-13 22:01:55 -0300

    Developed person create operation

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.exception.PersonNotFoundException;
+import one.digitalinnovation.personapi.repository.PersonRepository;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,9 +36,6 @@ public class PersonController {
     }
 
     @GetMapping
-    public String getBook() {
-        return "API Test!";
-    }
     public List<PersonDTO> listAll() {
         return personService.listAll();
     }
